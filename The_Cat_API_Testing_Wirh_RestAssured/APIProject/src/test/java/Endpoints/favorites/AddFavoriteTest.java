825478da6d8ae3a6c5921eef7830fcbd7921054e
Mohanad Ehab <139120908+Mohanadehab@users.noreply.github.com>
2024-10-15 18:01:23 +0300

    Add files via upload

---
+++ The_Cat_API_Testing_Wirh_RestAssured/APIProject/src/test/java/Endpoints/favorites/AddFavoriteTest.java
@@ -0,0 +1,31 @@
+package Endpoints.favorites;
+
+import Endpoints.BaseTest;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class AddFavoriteTest extends BaseTest {
+
+    public static String favId;
+
+    @Test(groups = "step1")
+    public void addFavorite() {
+        String imageId = "12300100-" ;
+        String xApiKey = "live_x0Ci0Nn0Nh0EF3wuTG457X6kxAk2y55ls2InkdKgFl4K70hLMDss0jrzv75bIn2E";
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .header("x-api-key", xApiKey)
+                .body("{\"image_id\": \"" + imageId + "\", \"sub_id\": \"my-user-1234\"}")
+                .when()
+                .post("/v1/favourites")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        favId = response.jsonPath().getString("id");
+
+    }
+
+}
